5fd3ee1e3cc9a11ead0de2d669c88570aaa613e3
crsmith01 <catsmith24601@gmail.com>
2023-11-07 13:34:48 -0500

    Update UserSearch to use class-based component

---
+++ src/classes/UserSearch.tsx
@@ -0,0 +1,51 @@
+import { Component, ReactNode } from 'react'
+
+interface User {
+    name: string
+    age: number
+}
+
+interface UserSearchProps {
+    users: User[]
+}
+
+interface UserSearchState {
+    name: string
+    user: User | undefined
+}
+
+class UserSearch extends Component<UserSearchProps> {
+   state: UserSearchState = {
+        name: '',
+        user: undefined
+   }
+
+    onClick = () => {
+        const foundUser = this.props.users.find((user) => {
+            return user.name === this.state.name
+        })
+
+        this.setState({ user: foundUser })
+
+        console.log(foundUser)
+    }
+
+   render() {
+    const { name, user } = this.state
+
+    return (
+        <>
+            User Search
+            <input value={name} onChange={e => this.setState({ name: e.target.value })}/>
+            <button onClick={this.onClick}>Find User</button>
+            <div>
+                {/* only try to access name property of user if user is defined */}
+                {user && user.name}
+                {user && user.age}
+            </div>
+        </>
+    )
+   }
+}
+
+export default UserSearch
